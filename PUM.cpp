aa802c5699d7ed03ed5aba686a0ea405c9739f48
saklayen-dev <saklayen@media365.com.bd>
2025-08-19 22:53:16 +0600

    Added new code

--- PUM.cpp
+++ PUM.cpp
@@ -10,7 +10,7 @@ int main()
     for (int i = 1; i <= n; i++)
     {
 
-        cout << count << " " << count + 1 << " " << count + 2 << endl;
+        cout << count << " " << count + 1 << " " << count + 2 << " "<< "PUM"<< endl;
         count += 4;
     }
 }
