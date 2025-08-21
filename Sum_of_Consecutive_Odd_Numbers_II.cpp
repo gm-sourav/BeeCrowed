206b53bc0ded53e07a0473b8afbab0783656e596
saklayen-dev <saklayen@media365.com.bd>
2025-08-21 23:40:17 +0600

    First Commit

---
+++ Sum_of_Consecutive_Odd_Numbers_II.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    while (n--)
+    {
+        int x, y;
+        cin >> x >> y;
+        int sum = 0;
+
+        if (x > y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        for (int i = x + 1; i < y; i++)
+        {
+            if (i % 2 != 0)
+            {
+                sum += i;
+            }
+        }
+        cout << sum << endl;
+    }
+}
